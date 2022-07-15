62bf907794302e7a33b96df3e6ad7c4728e86775
Ben Pham <benjamin.pham@gmail.com>
2022-07-15 18:11:54 +0200

    Revoved devise session flash messages

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  after_action :remove_notice, only: [:destroy, :create]
 
   # GET /resource/sign_in
   # def new
@@ -33,4 +34,10 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  private
+
+  def remove_notice
+    flash.discard(:notice)
+  end
 end
